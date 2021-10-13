4e3238c36211474593dc6432c38cb05ef3e20093
MathisVkg <mvk181@gmail.com>
2021-10-13 15:04:54 +0200

    fix router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
 import './assets/scss/App.css';
-import Loading from './components/Loading';
-import Movies from './components/Movies';
+// import Movies from './components/Movies';
 import NavBar from './components/NavBar';
 import Detail from './components/Detail';
 import Discover from './components/Discover';
 import Home from './components/Home';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=59d266ad02d1642bf64bc31fb887924c&page=';
 const PAGENUMBER = 1;
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+// const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-function MovieApi() {
-  const [loading, setLoading] = useState(true);
+export default function App() {
+
   const [movies, setMovies] = useState([]);
-
-  const fetchMovies = async () => {
-    setLoading(true);
-
+  const fetchMovies = async () =>  {
     try {
       const response = await fetch(APIURL + PAGENUMBER);
-      const movies = await response.json();
-      setLoading(false);
-      setMovies(movies);
-      console.log(movies);
+      const moviesData = await response.json();
+      setMovies(moviesData);
+      // console.log(moviesData);
+      // console.log(moviesData.results[0].title);
     } catch (error) {
-      setLoading(false);
       console.log(error);
     }
   }
   useEffect(() => {
     fetchMovies();
   }, []);
-
-
-  if(loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-}
-
-
-export default function App() {
-  MovieApi();
+  
+  
   return (
     <Router>
 
@@ -62,7 +45,7 @@ export default function App() {
           <Discover />
         </Route>
         <Route path='/'>
-          <Home />
+            <Home movies={ movies }/>
         </Route>
       </Switch>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
-
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+   
+const Home = (props) => {
+    if(props.movies.length != 0) { 
+        return (
+            <main>
+                <h1>Movie<span className="titleColor">Browser</span></h1>
+                <h2>{ props.movies.results[0].title }</h2>
+            </main>
+        )
+    } else {
+        return( 
+            <h2>Loading</h2>
+        )
+    }
 }
 
 export default Home
