8b3aecc76432c21652476c68fb1f0036daab54ad
MathisVkg <mvk181@gmail.com>
2021-10-18 21:41:33 +0200

    start user page

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,43 @@
-import React from 'react';
 import NavBar from './NavBar';
 import '../assets/scss/User.css';
+import React, { useEffect, useState } from 'react';
+//59d266ad02d1642bf64bc31fb887924c
 
 const User = () => {
+
+    const [getToken, setGetToken] = useState([]);
+    const [getUser, setGetUser] = useState([]);
+    const APITOKEN = 'https://api.themoviedb.org/3/authentication/token/new?api_key=59d266ad02d1642bf64bc31fb887924c';
+    const APIUSER = 'https://api.themoviedb.org/3/authentication/session/new?api_key=59d266ad02d1642bf64bc31fb887924c';
+
+    const fetchToken = async () =>  {
+        try {
+        const response = await fetch(APITOKEN);
+        const tokenData = await response.json();
+        setGetToken(tokenData);
+        } catch (error) {
+        console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchToken();
+    }, []);
+
+    const fetchUser = async () =>  {
+        try {
+        const response = await fetch(APIUSER);
+        const userData = await response.json();
+        setGetUser(userData);
+        } catch (error) {
+        console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchUser();
+    }, []);
+    console.log('1', getToken);
+    console.log('2', getUser);
+
     return (
         <>
             <BaseInfo />
@@ -14,7 +49,11 @@ const User = () => {
         return (
             <>
             <div className="container">
-                <img src="../assets/avatar.png" alt="avatar"></img>
+                <div>
+                    <p className="name">Mathis</p>
+                    <p className="lastname">Vankoekelberg</p>
+                    <img src="https://github.com/MathisVkg/movieBrowser/blob/main/src/assets/avatar.png?raw=true" alt="avatar"></img>
+                </div>
                 <p className="pseudo">Zeyros</p>
             </div>
             </>
